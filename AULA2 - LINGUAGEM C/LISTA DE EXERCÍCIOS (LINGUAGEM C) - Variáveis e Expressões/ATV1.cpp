f062803b6a29cae7e0e3ffa2fa9be62d736f5a4e
felipegomesoliveira546 <felipegomesoliveira23092010@gmail.com>
2026-08-30 00:20:00 -0300

    ATV1 e ATV2

---
+++ AULA2 - LINGUAGEM C/LISTA DE EXERCÍCIOS (LINGUAGEM C) - Variáveis e Expressões/ATV1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int x;
+	
+	printf("Digite um número:");
+	scanf("%d" , &x);
+	
+	printf("Valor de x: %d\n", x);
+	return 0;
+}
---
+++ AULA2 - LINGUAGEM C/LISTA DE EXERCÍCIOS (LINGUAGEM C) - Variáveis e Expressões/ATV2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	float x;
+	
+	printf("Escreva um número inteiro:");
+	scanf ("%f" ,&x);
+	
+	printf("Valor de x: %f\n", x);
+	return 0;
+}
